4a3ab13a348ad8ae92bb1657d624c9d610880c72
Devorah Ortega <devorahb.ortega@gmail.com>
2022-08-07 13:24:35 -0500

    Adds python problem

--- Practice1.py
+++ Practice1.py
@@ -1,13 +1,16 @@
 
 # 1. Use a variable to store a number, then write a condition that prints 0 if the number is equal to 10, and prints -1 otherwise.
+"""
 number = 5
 
 if number == 10:
   print(0)
 else:
   print(-1)
+"""
 
 # 2. Use a variable to store a number, then write a condition that prints -1 if the number is less than 10, prints 1 if the number is greater than 10, and prints 0 if the number is equal to 10.
+"""
 number = 11
 
 if number < 10:
@@ -16,8 +19,16 @@ elif number > 10:
     print(1)
 elif number == 10:
     print(0)
-    
+  """
+
 # 3. Use variables to store two numbers, then write a condition that prints 1 if the numbers are both less than 10, and prints 0 otherwise.
+number1 = 23
+number2 = 98
+
+if number1 < 10 and number2 < 10:
+  print(1)
+else:
+  print(0)
 
 # 4. Use a variable to store a number, then write a condition that prints 1 if the number is over 9000, and prints -1 otherwise.
 
