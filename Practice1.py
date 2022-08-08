012fa49f82bcc382446e3754e78cecca5c03e952
Devorah Ortega <devorahb.ortega@gmail.com>
2022-08-08 09:10:29 -0500

    Adds another python problem

--- Practice1.py
+++ Practice1.py
@@ -77,5 +77,12 @@ else:
  print(0)
 
 # 9. Use variables to store two numbers, then write a condition that prints 1 if the first number is less than zero and the second number is greater than 0, and prints 0 otherwise.
+number1 = 56
+number2 = 3
 
+if number1 < 0 and number2 > 0:
+  print(1)
+else:
+  print(0)
+  
 # 10. Use a variable to store a number, then write a condition that prints 5 if the number is greater than 80, prints 4 if the number is greater than 60, prints 3 if the number is greater than 40, prints 2 if the number is greater than 20, and prints 1 otherwise (only one print statement should occur).
