af905912758c72946dd0ff6b137af39061e1a0ba
Devorah Ortega <devorahb.ortega@gmail.com>
2022-08-09 17:33:25 -0500

    Adds solutions

--- Practice2.py
+++ Practice2.py
@@ -19,9 +19,14 @@ color1 = "blue"
 color2 = "red"
 color3 = "green"
 
-print("The three colors in this list are % s" % color1, "% s" % color2, "% s" % color3)
+print("The three colors in this list are " + color1 + ", " + color2 + ", " + color3 + ".")
 
 # 5. Write a program that uses variables to store three different colors, then prints out a sentence using the colors with string interpolation (the #{} operator).
+color1 = "blue"
+color2 = "red"
+color3 = "green"
+
+print("The three colors in this list are % s" % color1, "% s" % color2, "% s" % color3)
 
 # 6. Write a program that asks the user to enter a name. If the name is not "Santa", print "You're not Santa."
 
