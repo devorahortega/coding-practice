4d9b41f91503adffb2a4477e49a25325ccd7c43b
Devorah Ortega <devorahb.ortega@gmail.com>
2022-08-16 18:19:17 -0500

    adds arrays and a solution

--- Practice3.py
+++ Practice3.py
@@ -36,11 +36,18 @@ array[2] = array[2].capitalize
 print(array)
 
 # 6. Create an array to store 3 names. Then print out each name on separate lines with a while loop.
+array = ["Amy", "Joe", "Bob"]
 
 # 7. Create an array to store 2 strings. Then add one string to the array and print the array on one line.
+array = ["Apple", "Carrot"]
 
 # 8. Create an array to store 5 numbers. Then change the first number to 10 times its original value and print the array on one line.
+array = [1, 2, 3, 4, 7]
 
 # 9. Create an array to store 2 numbers. Then print out each number on separate lines with a while loop.
+array = [1, 3]
 
 # 10. Create an array to store names of 3 different countries. Then add one more country and print the array one line.
+array = ["Canada", "USA", "China"]
+
+print(array)
