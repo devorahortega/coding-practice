4d1796854dd9488cd449639e1578e8b71b4dfc1f
Devorah Ortega <devorahb.ortega@gmail.com>
2022-08-29 15:07:17 -0500

    Adds answer to problem

--- Practice4.rb
+++ Practice4.rb
@@ -1,6 +1,14 @@
 #  1. Start with an array of numbers and create a new array with each number times 3.
 #     For example, [1, 2, 3] becomes [3, 6, 9].
 
+array = [1, 2, 3]
+
+array.each do |n|
+    n = n*3
+end
+
+print array
+
 #  2. Start with an array of strings and create a new array with each string upcased.
 #     For example, ["hello", "goodbye"] becomes ["HELLO", "GOODBYE"].
 
