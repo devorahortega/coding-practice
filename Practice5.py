01ca22a49e018f5f6a683c577ce8b4c7515d881a
Devorah Ortega <devorahb.ortega@gmail.com>
2022-08-31 13:51:55 -0500

    Adds problems.

--- Practice5.py
+++ Practice5.py
@@ -1,4 +1,4 @@
-1. Start with an array of numbers and create a new array with only the numbers less than 20.
+#1. Start with an array of numbers and create a new array with only the numbers less than 20.
 #     For example, [2, 32, 80, 18, 12, 3] becomes [2, 18, 12, 3].
 
 #  2. Start with an array of strings and create a new array with only the strings that start with the letter "w".
