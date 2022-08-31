002ed2cf55c70d58338a186cd696cbd8998b78b8
Devorah Ortega <devorahb.ortega@gmail.com>
2022-08-31 13:51:24 -0500

    Adds different problems

--- Practice5.py
+++ Practice5.py
@@ -1,19 +1,29 @@
-# 1. Create an array to store 3 words. Then add two more words to the array and print the array on one line.
+1. Start with an array of numbers and create a new array with only the numbers less than 20.
+#     For example, [2, 32, 80, 18, 12, 3] becomes [2, 18, 12, 3].
 
-# 2. Create an array to store 4 letters. Then change the second letter to a number and print the array on one line.
+#  2. Start with an array of strings and create a new array with only the strings that start with the letter "w".
+#     For example, ["winner", "winner", "chicken", "dinner"] becomes ["winner", "winner"].
 
-# 3. Create an array to store 5 numbers. Then print out each number on separate lines with a while loop.
+#  3. Start with an array of hashes and create a new array with only the hashes with prices greater than 5 (from the :price key).
+#     For example, [{name: "chair", price: 100}, {name: "pencil", price: 1}, {name: "book", price: 4}] becomes [{name: "chair", price: 100}].
 
-# 4. Create an array to store 1 number. Then add three more numbers to the array and print the array on one line.
+#  4. Start with an array of numbers and create a new array with only the even numbers.
+#     For example, [2, 4, 5, 1, 8, 9, 7] becomes [2, 4, 8].
 
-# 5. Create an array to store 3 strings with lower case letters. Then change the third string to have all capital letters and print the array on one line.
+#  5. Start with an array of strings and create a new array with only the strings shorter than 4 letters.
+#     For example, ["a", "man", "a", "plan", "a", "canal", "panama"] becomes ["a", "man", "a", "a"].
 
-# 6. Create an array to store 3 names. Then print out each name on separate lines with a while loop.
+#  6. Start with an array of hashes and create a new array with only the hashes with names shorter than 6 letters (from the :name key).
+#     For example, [{name: "chair", price: 100}, {name: "pencil", price: 1}, {name: "book", price: 4}] becomes [{name: "chair", price: 100}, {name: "book", price: 4}].
 
-# 7. Create an array to store 2 strings. Then add one string to the array and print the array on one line.
+#  7. Start with an array of numbers and create a new array with only the numbers less than 10.
+#     For example, [8, 23, 0, 44, 1980, 3] becomes [8, 0, 3].
 
-# 8. Create an array to store 5 numbers. Then change the first number to 10 times its original value and print the array on one line.
+#  8. Start with an array of strings and create a new array with only the strings that don't start with the letter "b".
+#     For example, ["big", "little", "good", "bad"] becomes ["little", "good"].
 
-# 9. Create an array to store 2 numbers. Then print out each number on separate lines with a while loop.
+#  9. Start with an array of hashes and create a new array with only the hashes with prices less than 10 (from the :price key).
+#     For example, [{name: "chair", price: 100}, {name: "pencil", price: 1}, {name: "book", price: 4}] becomes [{name: "pencil", price: 1}, {name: "book", price: 4}].
 
-# 10. Create an array to store names of 3 different countries. Then add one more country and print the array one line.
+# 10. Start with an array of numbers and create a new array with only the odd numbers.
+#     For example, [2, 4, 5, 1, 8, 9, 7] becomes [5, 1, 9, 7].
