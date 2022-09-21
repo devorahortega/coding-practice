25a60803be9ae9eaabae2126eb49b5fda23dadf4
Devorah Ortega <devorahb.ortega@gmail.com>
2022-09-20 19:44:41 -0500

    Adds problem

---
+++ Practice6.py
@@ -0,0 +1,9 @@
+"""
+Write a function that returns the greatest value from an array of numbers.
+
+Input: [5, 17, -4, 20, 12]
+Output: 20
+
+(After you complete the exercise successfully, I recommend watching this video: https://anyonecanlearntocode.com/think-like-a-software-engineer/videos/50)
+"""
+
