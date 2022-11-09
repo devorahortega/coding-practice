929653e4c0cc03204c1ac7db4d1f0a621e5ef917
Devorah Ortega <devorahb.ortega@gmail.com>
2022-11-09 15:49:31 -0600

    Adds another attempt at an if loop in java

--- javapractice.java
+++ javapractice.java
@@ -1,13 +1,16 @@
 public class javapractice {
 public static void main(String[] args) {
-    int a = 3;
-    int b = 4;
+int a = 1;
+int b = 1;
 
-    if (a > b) {
-        System.out.println(a);
-    }
-    else {
-        System.out.println(b);
-    }
+if (a > b) {
+    System.out.println(a);
+}
+else if (a < b) {
+    System.out.print(b);
+}
+else {
+    System.out.println(a + b);
+}
 }
 }
