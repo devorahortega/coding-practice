d53a3811b7c98e2dacabea18e6fc500dd0216fbf
Devorah Ortega <devorahb.ortega@gmail.com>
2022-11-09 15:52:52 -0600

    Attempts to learn a while loop

--- javapractice.java
+++ javapractice.java
@@ -1,16 +1,10 @@
 public class javapractice {
-public static void main(String[] args) {
-int a = 1;
-int b = 1;
+public static void main (String[] args) {
+    int i = 1;
 
-if (a > b) {
-    System.out.println(a);
-}
-else if (a < b) {
-    System.out.print(b);
-}
-else {
-    System.out.println(a + b);
-}
+    do { System.out.println(i); 
+        i++;
+    }
+    while (i > 10);
 }
 }
