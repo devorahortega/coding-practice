df538c7337b8c917d1ea0a9b7904c42cfaf5a58f
Devorah Ortega <devorahb.ortega@gmail.com>
2022-11-09 15:46:57 -0600

    Adds first attempt at loop

--- javapractice.java
+++ javapractice.java
@@ -1,5 +1,13 @@
 public class javapractice {
-public static void main() {
-    System.out.println("Hello world!");
+public static void main(String[] args) {
+    int a = 3;
+    int b = 4;
+
+    if (a > b) {
+        System.out.println(a);
+    }
+    else {
+        System.out.println(b);
+    }
 }
 }
