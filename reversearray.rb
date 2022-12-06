e26cc6aae11667c20b6e37b39ae560813636bf59
Devorah Ortega <devorahb.ortega@gmail.com>
2022-12-06 14:40:13 -0600

    Adds solution to array reverse

--- reversearray.rb
+++ reversearray.rb
@@ -2,3 +2,17 @@
 
 # Input: [1, 2, 3, 4, 5]
 # Output: [5, 4, 3, 2, 1]
+
+def reverse(array)
+    index = array.length - 1
+    result = []
+
+    while index >= 0
+        result.push(array[index])
+        index -= 1
+    end
+
+    return result
+end
+
+p reverse([1, 2, 3, 4, 5])
