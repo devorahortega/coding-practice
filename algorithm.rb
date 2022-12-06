faecfa5487dd687c199c14201d52412a322a24d7
Devorah Ortega <devorahb.ortega@gmail.com>
2022-12-06 14:23:17 -0600

    Solves for array

--- algorithm.rb
+++ algorithm.rb
@@ -1,4 +1,26 @@
-#Write a function that returns the reverse of a given string.
+# Write a function that returns the reverse of a given string.
 
-#Input: “abcde”
-#Output: “edcba”
+# Input = ["h", "e", "l", "l", "o"]
+# Output: ["o", "l", "l", "e", "h"]
+
+def reverse(array)
+    index = array.length - 1
+    result = []
+
+    while index >= 0
+        result.push(array[index])
+        index -= 1
+    end
+
+    return result
+end 
+
+p reverse(["h", "e", "l", "l", "o"])
+
+# Define the string 
+# create a loop .each do
+# shift first to last until string is done
+
+
+# Input: "hello" 
+# Output: "olleh"
