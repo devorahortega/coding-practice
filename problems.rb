cb629e72b1085dd721ca9295b4dd688174109b53
Devorah Ortega <devorahb.ortega@gmail.com>
2022-12-07 16:07:30 -0600

    Solves problem

--- problems.rb
+++ problems.rb
@@ -1,5 +1,11 @@
 # 1. Write a function that takes in a number and returns the number times two. Then run the function and print the result.
 
+def doubled(num)
+    puts num * 2
+end
+
+return doubled(2)
+
 # 2. Write a function that takes in a string and returns the string with all capital letters. Then run the function and print the result.
 
 # 3. Write a function that takes in two numbers and returns the first number subtracted by the second. Then run the function and print the result.
