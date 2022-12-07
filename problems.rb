2aa00da9ba994e4aa5daaf89cd633d39689757de
Devorah Ortega <devorahb.ortega@gmail.com>
2022-12-07 16:10:56 -0600

    Solves problem

--- problems.rb
+++ problems.rb
@@ -29,3 +29,9 @@ p all_caps("hello")
 # 9. Write a function that takes in 3 numbers and returns the average (the sum divided by 3.0). Then run the function and print the result.
 
 # 10. Write a function that takes in a number and returns the number times 10 plus 30. Then run the function and print the result.
+
+def maths(num)
+    return num * 10 + 30
+end
+
+p maths(10)
