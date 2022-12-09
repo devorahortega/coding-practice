6ee524168275f51cf50825cbae9789278b4c57fe
Devorah Ortega <devorahb.ortega@gmail.com>
2022-12-08 20:21:16 -0600

    adds solution

--- methods.rb
+++ methods.rb
@@ -1,24 +1,34 @@
 # 1. Write a program that asks the user to enter a word, then prints that word with all capital letters.
 
-puts "Please enter a word:"
+# puts "Please enter a word:"
 
-word = gets.chomp.upcase
+# word = gets.chomp.upcase
 
-puts word
+# puts word
 
 # 2. Write a program that asks the user to enter a number, then prints "That's a big number" if the number is greater than 100.
 
-puts "Please enter a number:"
+# puts "Please enter a number:"
 
-num = gets.chomp
+# num = gets.chomp
 
-if num.to_i > 100
-    p "That's a big number!"
-else
-    p num
-end
+# if num.to_i > 100
+#     p "That's a big number!"
+# else
+#     p num
+# end
 
 # 3. Write a program that asks the user to enter two numbers, then prints the numbers added together.
+
+puts "Please enter a number:"
+
+num1 = gets.chomp
+
+puts "Please enter another number:"
+
+num2 = gets.chomp
+
+puts num1.to_i + num2.to_i
 
 # 4. Write a program that asks the user to enter a word, then prints that word in reverse order.
 
