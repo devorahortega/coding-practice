6c53b2da7b70b4cac33654b05a516b8bb3dcf6db
Devorah Ortega <devorahb.ortega@gmail.com>
2022-12-08 20:18:22 -0600

    adds solution

--- methods.rb
+++ methods.rb
@@ -8,7 +8,15 @@ puts word
 
 # 2. Write a program that asks the user to enter a number, then prints "That's a big number" if the number is greater than 100.
 
+puts "Please enter a number:"
 
+num = gets.chomp
+
+if num.to_i > 100
+    p "That's a big number!"
+else
+    p num
+end
 
 # 3. Write a program that asks the user to enter two numbers, then prints the numbers added together.
 
